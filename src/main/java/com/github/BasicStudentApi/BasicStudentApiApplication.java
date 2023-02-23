b6e41cba39be2a79b13deb9b3d539df0232c6d06
Carlos <carlosj616@outlook.com>
2023-02-23 13:17:05 -0500

    Initial commit

---
+++ src/main/java/com/github/BasicStudentApi/BasicStudentApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.BasicStudentApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicStudentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicStudentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/BasicStudentApi/BasicStudentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.BasicStudentApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicStudentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
